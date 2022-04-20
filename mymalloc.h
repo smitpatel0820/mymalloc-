89ec93ef21315c52aebe2ac1e97a9f4593f04b60
smitpatel0820 <104075063+smitpatel0820@users.noreply.github.com>
2022-04-20 08:18:55 -0400

    Delete mymalloc.h

--- mymalloc.h
+++
@@ -1,9 +0,0 @@
-#include <sys/types.h>
-
-void memoryIntialize();
-
-void *mymalloc(size_t size, const char *file, const int lineNum);
-void myfree(void *vpointer, const char *file, const int lineNum);
-
-#define malloc(x) mymalloc(x, __FILE__, __LINE__)
-#define free(x) myfree(x, __FILE__, __LINE__)
